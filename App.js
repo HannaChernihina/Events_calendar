faffb0e4b0e6d17005a60afa38e865d7b6c238c0
HannaChernihina <hanna_chernihina@ukr.net>
2019-07-24 23:47:53 +0300

    add icons

--- App.js
+++ App.js
@@ -26,14 +26,20 @@ function app() {
         const tdName = document.createElement('td');
         const tdStartDate = document.createElement('td');
         const tdEndDate = document.createElement('td');
+        const tdIconEdit = document.createElement('td');
+        const tdClose = document.createElement('td');
 
         tdStartDate.innerText = `${startDate.getHours()}:${startDate.getMinutes()}`;
         tdEndDate.innerText = `${startDate.getHours()}:${startDate.getMinutes()}`;
+        tdIconEdit.innerHTML = '<i class="far fa-edit"></i>';
+        tdClose.innerHTML = '<i class="fas fa-times"></i>';
         tdName.innerText = name;
 
         tr.appendChild(tdName);
         tr.appendChild(tdStartDate);
         tr.appendChild(tdEndDate);
+        tr.appendChild(tdIconEdit);
+        tr.appendChild(tdClose);
 
         tbody.appendChild(tr);
     })
