f58598c981cfe2a426abe6200e473b85154e4f10
HannaChernihina <hanna_chernihina@ukr.net>
2019-08-30 22:27:45 +0300

    edit view

--- app_js_new.js
+++ app_js_new.js
@@ -1,55 +1,82 @@
 let tasks = [];
 
+// получаем значения в инпутах
+function pushInfo() {
+    // получаем значения в инпутах, записываем в обект в массиве
+    let inputName = document.getElementById('inName').value;
+    let inputDate = document.getElementById('inDate').value;
+    let inputTimeStart = document.getElementById('inTimeStart').value;
+    let inputTimeEnd = document.getElementById('inTimeEnd').value;
+
+    //if (inputName.length)
+
+    tasks.push({
+        taskName: inputName,
+        date: inputDate,
+        timeStart: inputTimeStart,
+        timeEnd: inputTimeEnd,
+    });
+
+// очищаем инпут после записи в обьект
+    document.getElementById('inName').value = '';
+    document.getElementById('inDate').value = '';
+    document.getElementById('inTimeStart').value = '';
+    document.getElementById('inTimeEnd').value = '';
+}
+
+
+
+
+
+// отрисовка таблицы
+function dataOutput() {
+    let eventsBlock = document.getElementById("events");
+
+    eventsBlock.innerHTML = '';
+
+    for (let i = 0; i < tasks.length; i++) {
+        let task = tasks[i];
+
+        let spanName = document.createElement('span');
+        spanName.className = 'informationCeil';
+        spanName.innerHTML = task.taskName;
+
+        let spanDate = document.createElement('span');
+        spanDate.className = 'informationCeil';
+        spanDate.innerText = task.date;
+
+        let spanTimeStart = document.createElement('span');
+        spanTimeStart.className = 'informationCeil';
+        spanTimeStart.innerText = task.timeStart;
+
+        let spanTimeEnd = document.createElement('span');
+        spanTimeEnd.className = 'informationCeil';
+        spanTimeEnd.innerText = task.timeEnd;
+
+        let divElement = document.createElement('div');
+        divElement.className = "list-group-item";
+
+        divElement.append(spanName);
+        divElement.append(spanDate);
+        divElement.append(spanTimeStart);
+        divElement.append(spanTimeEnd);
+
+        eventsBlock.append(divElement);
+    }
+};
+
+
 window.onload = function () {
     let wprapItemInput = document.getElementById('add');
 
+
+    // обработчик события по клику
     wprapItemInput.addEventListener('click', event => {
-        let inputName = document.getElementById('inName').value,
-            inputDate = document.getElementById('inDate').value,
-            inputTimeStart = document.getElementById('inTimeStart').value,
-            inputTimeEnd = document.getElementById('inTimeEnd').value;
-
-        //if (inputName.length)
-
-        tasks.push({
-            taskName: inputName,
-            date: inputDate,
-            timeStart: inputTimeStart,
-            timeEnd: inputTimeEnd
-        });
+// запись инфы в обьект - обекты в массив
+        pushInfo();
+        // отрисовка таблицы
+        dataOutput();
 
 
-
-        let eventsBlock = document.getElementById("events");
-
-        eventsBlock.innerHTML = '';
-        for (let i = 0; i < tasks.length; i++) {
-            let task = tasks[i];
-
-            let spanName = document.createElement('span');
-            spanName.className = 'informationCeil';
-            spanName.innerHTML = task.taskName;
-
-            let spanDate = document.createElement('span');
-            spanDate.className = 'informationCeil';
-            spanDate.innerText = task.date;
-
-            let spanTimeStart = document.createElement('span');
-            spanTimeStart.className = 'informationCeil';
-            spanTimeStart.innerText = task.timeStart;
-
-            let spanTimeEnd = document.createElement('span');
-            spanTimeEnd.className = 'informationCeil';
-            spanTimeEnd.innerText = task.timeEnd;
-
-            let divElement = document.createElement('div');
-
-            divElement.append(spanName);
-            divElement.append(spanDate);
-            divElement.append(spanTimeStart);
-            divElement.append(spanTimeEnd);
-
-            eventsBlock.append(divElement);
-        }
     });
 }
